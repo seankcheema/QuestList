0d987e1c80884abb14ee2dfab4c4d759a843794c
Richard <goldpugz@gmail.com>
2023-01-23 15:26:11 -0500

    added routing navbar pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { CommunityComponent } from './community/community.component';
+import { HomeComponent } from './home/home.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { TopGamesComponent } from './top-games/top-games.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
+  { path: 'top-games', component: TopGamesComponent},
+  { path: 'community', component: CommunityComponent},
+  { path: 'about-us', component: AboutUsComponent},
+  { path: 'sign-up', component: SignUpComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/community/community.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-community',
+  templateUrl: './community.component.html',
+  styleUrls: ['./community.component.css']
+})
+export class CommunityComponent {
+
+}
