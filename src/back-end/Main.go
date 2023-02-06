8fe7bcf851ed66efa7d8c292c674f2e79feb6de5
Javier <lopez.j@ufl.edu>
2023-02-05 20:26:25 -0500

    Created proper function for basic handle requests used for testing and video

--- src/back-end/Main.go
+++ src/back-end/Main.go
@@ -1,7 +1,5 @@
 package main
 
-//This is a test code to ensure that back end can retrieve url handles and process them
-
 import (
 	"fmt"
 	"net/http"
@@ -20,54 +18,61 @@ func main() {
 	//Creates a rounter
 	router := mux.NewRouter()
 
-	// //Create RAWG SDK config and client
-	// config := rawg.Config{
-	// 	ApiKey:   "476cd66f8e4d44eb975aad199e0d7a07", //RAWG API key
-	// 	Language: "en",                               // English
-	// 	Rps:      5,                                  // Has to stay 5 (limit)
-	// }
-	// client := rawg.NewClient(http.DefaultClient, &config)
-
-	// _ = client
-	//specify endpoints, handlers, and HTTP methods
-	router.HandleFunc("/games", PrintGames).Methods("GET")
+	router.HandleFunc("/specific-game", PrintGames).Methods("GET")
+	router.HandleFunc("/allGames", PrintAllGames).Methods("GET")
+	router.HandleFunc("/", Hello).Methods("GET")
 	http.Handle("/", router)
 
 	//Start and listen for requests
 	http.ListenAndServe(":8080", router)
 
-	// RAWG SDK
+}
 
-	// filter := rawg.NewGamesFilter().SetPageSize(40)
-	// var games []*rawg.Game
-	// var num int
-	// var err error
-	// games, num, err = client.GetGames(filter)
-	// limit := num
-	// var j int = 1
-	// for limit > 0 {
-	// 	for i := 0; i < 40; i++ {
-	// 		fmt.Println(games[i].Name)
-	// 	}
-	// 	j++
-	// 	filter := rawg.NewGamesFilter().SetPage(j).SetPageSize(40)
-	// 	games, num, err = client.GetGames(filter)
-	// 	limit -= 40
-	// }
-
-	// _ = err
-	// _ = num
-	// _ = games
-
-	// if err != nil {
-
-	// } else {
-	// 	fmt.Println(err)
-	// }
-
+func Hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, Welcome to the Temporary Back-End Home Page")
 }
 
 func PrintGames(w http.ResponseWriter, r *http.Request) {
+	//Specify status code
+	w.WriteHeader(http.StatusOK)
+
+	//Create RAWG SDK config and client
+	config := rawg.Config{
+		ApiKey:   "476cd66f8e4d44eb975aad199e0d7a07", //RAWG API key
+		Language: "en",                               // English
+		Rps:      5,                                  // Has to stay 5 (limit)
+	}
+
+	//Setup client to talk to database
+	client := rawg.NewClient(http.DefaultClient, &config)
+
+	fmt.Println("Input game name:")
+
+	var err error
+
+	var name string
+
+	fmt.Scanln(&name)
+
+	//Update response writer
+	filter := rawg.NewGamesFilter().SetPageSize(40).SetSearch(name)
+	var games []*rawg.Game
+	var num int
+	games, num, err = client.GetGames(filter)
+	for i := 0; i < 10; i++ {
+		fmt.Fprint(w, "Name: ")
+		fmt.Fprintln(w, games[i].Name)
+		// fmt.Fprint(w, "Rating: ")
+		// fmt.Fprintln(w, games[i].Rating)
+	}
+
+	_ = err
+	_ = num
+	_ = games
+
+}
+
+func PrintAllGames(w http.ResponseWriter, r *http.Request) {
 	//Specify status code
 	w.WriteHeader(http.StatusOK)
 	//Create RAWG SDK config and client
@@ -76,19 +81,31 @@ func PrintGames(w http.ResponseWriter, r *http.Request) {
 		Language: "en",                               // English
 		Rps:      5,                                  // Has to stay 5 (limit)
 	}
+
+	//Setup client to talk to databse
 	client := rawg.NewClient(http.DefaultClient, &config)
 
-	//Update response writer
-	filter := rawg.NewGamesFilter().SetPageSize(40).SetSearch("Overwatch 20")
+	//Update response writer and request all games
+	filter := rawg.NewGamesFilter().SetPageSize(40)
 	var games []*rawg.Game
 	var num int
 	var err error
+
 	games, num, err = client.GetGames(filter)
-	for i := 0; i < 10; i++ {
-		fmt.Fprint(w, "Name: ")
-		fmt.Fprintln(w, games[i].Name)
-		// fmt.Fprint(w, "Rating: ")
-		// fmt.Fprintln(w, games[i].Rating)
+	limit := num
+	var j int = 1
+
+	//Limit of 40 games per "page" so we iterarte through all pages
+	for limit > 0 {
+		for i := 0; i < 40; i++ {
+			fmt.Fprint(w, "Name: ")
+			fmt.Fprintln(w, games[i].Name)
+		}
+
+		j++
+		filter := rawg.NewGamesFilter().SetPage(j).SetPageSize(40)
+		games, num, err = client.GetGames(filter)
+		limit -= 40
 	}
 
 	_ = err
