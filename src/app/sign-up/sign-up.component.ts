d8c522011db0d411276a9750db802dd310fe11c9
tomgallego13 <93276519+tomgallego13@users.noreply.github.com>
2023-02-26 17:15:52 -0500

    Update sign-up.component.ts

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms'
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent {
+export class SignUpComponent{
+  constructor(private formBuilder:FormBuilder){}
+  
+  profileForm = this.formBuilder.group({
+    username:[''],
+    email:[''],
+    password:[''],
+    dob:['']
+  })
 
+  signup() {
+    console.log('Form data is ', this.profileForm.value)
+  }
 }
