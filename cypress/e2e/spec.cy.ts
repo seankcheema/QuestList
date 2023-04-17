e0bfef5b0b09b55748bb1a242fe90382837b9576
Richard <goldpugz@gmail.com>
2023-04-17 12:32:10 -0400

    added cypress testing for new features

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,4 +1,15 @@
 describe('template spec', () => {
+
+  /**
+   * Clicking About Us should bring you to about us page
+   */
+  it('should go to about us page', () => {
+    cy.visit('localhost:4200')
+
+    cy.get('button').contains('About Us').click()
+
+    cy.url().should('include', '/about-us')
+  });
   
   /**
    * Test that the top games page displays games
@@ -7,6 +18,22 @@ describe('template spec', () => {
     cy.visit('localhost:4200')
 
     cy.contains('Top Games').click()
+
+    cy.contains('Grand Theft Auto V')
+  });
+
+  /**
+   * Test that the top games page buttons work
+   */
+  it('top games buttons should work', () => {
+
+    cy.visit('localhost:4200/top-games')
+
+    cy.get('button').contains('Next 40 >').click()
+
+    cy.contains('Apex Legends')
+
+    cy.get('button').contains('< Previous 40').click()
 
     cy.contains('Grand Theft Auto V')
   });
@@ -25,14 +52,11 @@ describe('template spec', () => {
     cy.get('input[formControlName="email"]').type('admin@localhost', {force: true})
     cy.get('input[formControlName="password"]').type('password', {force: true})
 
-    const stub = cy.stub()
-
-    cy.on('window:alert', stub)
-
     cy.get('button').contains('Sign Up').click()
-    .then(() => {
-      expect(stub.getCall(0)).to.be.calledWith("This user already exists")
-    })
+
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('This user already exists');
+    });
 
   });
 
@@ -50,6 +74,10 @@ describe('template spec', () => {
     cy.get('.btn').click()
 
     cy.url().should('include', '/home')
+
+    cy.get('img[class="profile-image"]').click()
+    cy.get('button').contains('Profile').click()
+    cy.url().should('include', '/user')
   });
 
   /**
